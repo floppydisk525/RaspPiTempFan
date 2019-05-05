e8663d99538a0c99559d493f9339e8f532a07109
floppydisk525 <mark@ijohnsen.com>
2019-05-05 11:12:38 -0700

    add a few command line statements back in timer

--- cprog/tempio.c
+++ cprog/tempio.c
@@ -211,6 +211,16 @@ void DS18ReadCheckTime ()
 		//DS18ReadTemp ();     //read value of temp HERE
 		DS18SecCnt = 0;
 		//DS18GetTimeStartFlag = 0;  //note - this can be skipped.
+		
+		//following lines print to command line to show reading temp
+		//DELETE when ready.
+		printf("READ TEMP TIMER\n");
+		printf("----------- system time ------------\n");
+		time_t t = time(NULL);
+		struct tm tm = *localtime(&t);
+		
+		printf("now: %d-%d-%d %d:%d:%d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+		printf("\n----------------------------------\n");
 	}
 }
 
