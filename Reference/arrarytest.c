ef1196204d7c9eefa5fa6065fb5954c3a263d4e2
floppydisk525 <mark@ijohnsen.com>
2019-05-29 22:01:26 -0700

    arraytest.c added int function, commented everything else out

--- Reference/arrarytest.c
+++ Reference/arrarytest.c
@@ -1,3 +1,7 @@
+//Build and run this program with something like:
+//gcc -Wall arraytest.c -o arraytest
+//sudo ./arraytest
+
 #include <stdio.h>
 #include <string.h>
 
@@ -20,12 +24,23 @@ void fillarray ( char ** array, int SIZE){
 	
 }
 
+int getTempSensCnt () {
+	int cnt = 3;
+	return cnt;
+}
+
 int main( void ){
-    char array[][50];
-    int SIZE;
+//    char array[][50];
+//    int SIZE;
+	
+	//int tempSensCnt;
+	
+	int tempSensCnt = getTempSensCnt();
+	printf ("The count is: %d\n", tempSensCnt);
 
 //    ...call function to fill array... this part works.
-	fillarray( array, SIZE);
-    printarray( array, SIZE );
+	//fillarray( array, SIZE);
+    //printarray( array, SIZE );
+	
 }
 
